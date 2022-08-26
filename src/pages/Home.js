8e05d815b31692183840270131d0167d79bd3156
LucasAccurcio <lucas.accurcio@gmail.com>
2022-08-25 23:58:08 -0300

    style: :art: create css to HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Olá
+    <div className='home-container'>
+      <main>
+        <h1>Olá Dra. Érica!</h1>
+        <br />
+        <br />
+        <h3>O que gostaria de fazer?</h3>
+      </main>
+      <nav className='home-buttons-container'>
+        <button onClick={ () => navigate('/service') }>Cadastrar serviço</button>
+        <button onClick={ () => navigate('/report') }>Gerar relatório</button>
+      </nav>
     </div>
   );
 }
