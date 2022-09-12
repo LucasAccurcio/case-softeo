6d9b399b04c4a5dd525d7d18780cf9d33a517669
LucasAccurcio <lucas.accurcio@gmail.com>
2022-09-12 19:08:44 -0300

    style: add data-testid to the button return

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,16 +8,22 @@ const Navbar = (props) => {
   const navigate = useNavigate();
 
   const arrowLeft = (<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
-  <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-</svg>);
+    <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+  </svg>);
 
   const btnEmpty = (
     <button className='empty-button'> </button>
   );
 
   return (
-    <nav className='navbar-container'>
-      <button className='return-button' onClick={() => { navigate('/home') }}>{ arrowLeft }</button>
+    <nav className="navbar-container">
+      <button
+        data-testid="back-button"
+        className="return-button"
+        onClick={() => { navigate('/home') }}
+      >
+        { arrowLeft }
+      </button>
       <h3>{ title }</h3>
       { btnEmpty }
     </nav>
