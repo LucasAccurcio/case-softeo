b492f9ef27c6c4dde927d7e662004813df3bfa51
LucasAccurcio <lucas.accurcio@gmail.com>
2022-09-13 02:48:12 -0300

    feat: :sparkles: add route to manage services

--- src/router/Router.js
+++ src/router/Router.js
@@ -3,12 +3,14 @@ import Home from '../pages/Home';
 import Service from '../pages/Service';
 import NotFound from '../pages/NotFound';
 import Report from '../pages/Report';
+import Manage from '../pages/Manage';
 
 
 const Router = () => (
   <Routes>
     <Route exact path="/home" element={ <Home /> } />
     <Route exact path="/service" element={ <Service /> } />
+    <Route exact path="/manage" element={ <Manage /> } />
     <Route exact path="/report" element={ <Report /> } />
     <Route exact path="/" element={ <Navigate to="/home" /> } />
     <Route path="*" element={ <NotFound /> } />
